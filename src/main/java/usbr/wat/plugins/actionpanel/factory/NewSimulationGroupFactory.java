31de743cf69598685db197620f3eb6a6fe98a7c4
Drew Allan Loney <dloney@usbr.gov>
2026-06-04 12:40:56 -0600

    Commented factory code

--- src/main/java/usbr/wat/plugins/actionpanel/factory/NewSimulationGroupFactory.java
+++ src/main/java/usbr/wat/plugins/actionpanel/factory/NewSimulationGroupFactory.java
@@ -1,38 +1,58 @@
-/*
- * Copyright 2021  Hydrologic Engineering Center (HEC).
- * United States Army Corps of Engineers
- * All Rights Reserved.  HEC PROPRIETARY/CONFIDENTIAL.
- * Source may not be released without written approval
- * from HEC
- */
 package usbr.wat.plugins.actionpanel.factory;
 
-import com.rma.commands.AbstractNewManagerCommand;
-import com.rma.factories.AbstractNewManagerFactory;
-import com.rma.io.RmaFile;
-import com.rma.model.Project;
-import com.rma.util.I18n;
+import com.rma.commands.AbstractNewManagerCommand;  // Base class for RMA commands that create new Manager objects in the project
+import com.rma.factories.AbstractNewManagerFactory; // Base factory class that drives the new-manager creation workflow in the RMA framework
+import com.rma.io.RmaFile;                          // RMA abstraction representing the file or directory where the new manager's data will be stored
+import com.rma.model.Project;                       // Represents the currently loaded RMA project into which the new manager will be registered
+import com.rma.util.I18n;                           // Internationalization object carrying the display name and description for this factory
 
 /**
- * @author Mark Ackerman
+ * Factory for creating new SimulationGroup manager objects within the WTMP
+ * Action Panel.
+ *
+ * Extends AbstractNewManagerFactory to integrate with the RMA framework's
+ * new-manager creation workflow, which handles UI presentation, name/description
+ * prompting, and command execution on behalf of the factory.
+ *
+ * The createCommand() method is currently a stub returning null and is intended
+ * to be implemented with a concrete AbstractNewManagerCommand subclass that
+ * constructs and registers a new SimulationGroup in the project.
  *
  */
-public class NewSimulationGroupFactory extends AbstractNewManagerFactory
-{
+public class NewSimulationGroupFactory extends AbstractNewManagerFactory {
 	/**
-	 * @param info
+	 * Constructs a NewSimulationGroupFactory with the given internationalization info.
+	 *
+	 * Passes the I18n object to the AbstractNewManagerFactory superclass, which
+	 * uses it to provide the factory's display name and description within the
+	 * RMA framework's new-manager selection UI.
+	 *
+	 * @param info the I18n object carrying the localized name and description for this factory
 	 */
-	public NewSimulationGroupFactory(I18n info)
-	{
+	public NewSimulationGroupFactory(I18n info) {
+		// Delegate to the base factory with the localized display information
 		super(info);
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated catch block
 	}
 
+	/**
+	 * Creates and returns the command responsible for constructing a new SimulationGroup.
+	 *
+	 * This method is called by the RMA framework after the user has supplied a name,
+	 * description, and storage location for the new manager. It is currently a stub
+	 * returning null and must be implemented with a concrete AbstractNewManagerCommand
+	 * subclass (such as NewSimulationGroupCmd) that creates and registers the
+	 * SimulationGroup in the project.
+	 *
+	 * @param proj the Project into which the new SimulationGroup will be registered
+	 * @param name the user-entered name for the new SimulationGroup
+	 * @param desc the user-entered description for the new SimulationGroup
+	 * @param file the RmaFile representing the storage location for the group's data
+	 * @return the command to execute for creating the SimulationGroup, or null if not yet implemented
+	 */
 	@Override
-	protected AbstractNewManagerCommand createCommand(Project proj, String name,
-			String desc, RmaFile file)
-	{
+	protected AbstractNewManagerCommand createCommand(Project proj, String name, String desc, RmaFile file) {
+		// TODO: Return a concrete NewSimulationGroupCmd instance to complete the creation workflow
 		return null;
 	}
-	
 }
